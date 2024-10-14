b06abf23db0c6522c33302e4538f52aa41cfb896
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-10-14 16:05:38 +0200

    [UPD]creato controller API

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/test', function () {
+    return response()->json([
+        'name' => 'endriu',
+        'surname' => 'kaskija'
+    ]);
+})->name('test_api');
