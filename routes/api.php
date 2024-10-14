a2bc58c66827e286839ca30c320e9c5cbd21ca8f
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-10-14 16:24:06 +0200

    [UPD]fine milestone 2

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\GuestController as GuestController;
+use App\Http\Controllers\Api\PostController as PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/hello', [GuestController::class, 'hello'])->name('guest_home');
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
 
 // Route::get('/test', function () {
 //     return response()->json([
